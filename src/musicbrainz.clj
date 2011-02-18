1e5abc5a0f4f47cfd01cdae60412beeb63541550
Tristan King <tristan.king@gmail.com>
2011-02-18 17:17:56 +0100

    still working on getting things stable

--- src/musicbrainz.clj
+++ src/musicbrainz.clj
@@ -1,4 +1,5 @@
 (ns musicbrainz
+  (:require musicbrainz.artists)
   (:require [clojure.contrib.string :as s])
   (:require [clj-http.client :as c]))
 
@@ -20,8 +21,10 @@
 	      (= (xml :tag) :event)
 	      (= (xml :tag) :label))
 	  (let [a (xml :attrs)
+		a (if (contains? a :ext:score)
+		    (assoc a :ext:score (Integer/parseInt (a :ext:score)))
+		    a)
 		contents (map xml2map (xml :content))]
-	    ;(println a " - " contents))
 	    (apply merge a contents))
 	  (or (= (xml :tag) :title)
 	      (= (xml :tag) :asin)
@@ -43,10 +46,34 @@
 	  )))
 
 (defn search [resource params]
-  (let [url (str base-url (name resource) "/?type=xml&" (s/join "&" (for [[k v] params] (str (name k) "=" (s/replace-str " " "+" v)))))
-	r (get-xml url)
-	m (xml2map r)]
-    (or m {})))
+  (let [url (str base-url (name resource) "/?type=xml&" (s/join "&" (for [[k v] params] (str (name k) "=" (s/replace-str " " "+" (str v))))))]
+    (. System/out (println url))
+    (let [r (loop [try 0]
+	      (if (> try 4)
+		{}
+		(let [r (try
+			 (get-xml url)
+			 (catch Exception e
+			   {:error e}))]
+		  (if (contains? r :error)
+		    (do
+		      (. System/out (println "error: retrying in 1 second"))
+		      (. Thread (sleep 1000))
+		      (recur (inc try)))
+		    r))))
+	  cnt (Integer/parseInt (get-in (first (r :content)) [:attrs :count] "0"))
+	  offset (get params :offset 0)
+	  limit (get params :limit 25)
+	  m (xml2map r)]
+      (merge-with concat
+		  (or m {})
+		  (if (and (> cnt (+ offset limit))
+			   (= 100 (get (last (val (first m))) :ext:score 0)))
+			   ; if there are more search results, get them too, 
+                           ; but only if they potentially have a score of 100
+		    (search resource (assoc params
+				       :offset (+ offset limit)))
+		    {})))))
 
 (defn id3-to-map [id3]
   {:artist (.getArtist id3)
@@ -81,16 +108,39 @@
   (get (first (sort-by :date (i :release-event-list))) :date ""))
 
 (defn run [dir]
-  (sort-by
-   date-keyfn
-   (apply
-    concat
-    (map (fn [artist]
-	   (filter #(= (% :ext:score) "100")
-		   ((search :release {:artist artist}) :release-list)))
-	 (set (map :artist (mp3scan dir)))))))
+  (reverse
+   (sort-by
+    date-keyfn
+    (distinct ; to make sure we don't have the same result twice
+     (apply
+      concat
+      (map (fn [artist]
+	     (filter #(= (% :ext:score) 100)
+		     ((search :release {:artist artist}) :release-list)))
+	   (remove ; TODO: are there any other equivatents of this?
+	    #(= "various artists" %)
+	    (distinct
+	     (map 
+	      #(.toLowerCase %)
+	  ;(map :artist (mp3scan dir))))))))
+	  ; TODO: filter out invalid artist names
+	  ; using hack until this is fixed
+	      musicbrainz.artists/artists)))))))))
 
-(defn report [dir]
-  (doseq [r (run dir)]
-    (println (r :name) "-" (r :title) (str "(" (r :type) ")")))
-  )
+(defn scan-mp3s-for-artist [dir]
+  (spit
+   "artists.clj"
+   (set (map :artist (mp3scan dir)))))
+
+(defn full-report [dir]
+  (spit 
+   "report.html"
+   (str 
+    "<html><body><table>\n"
+    "<tr><th>Artist</th><th>Album</th><th>Type</th><th>Released</th></tr>\n"
+    (apply 
+     str
+     (for [r (filter #(re-find #"^(Album|EP) Official$" (% :type)) (run dir))]
+       (str "<tr><td>" (r :name) "</td><td>" (r :title) "</td><td>" (r :type)
+	    "</td><td>" (date-keyfn r) "</td></tr>\n")))
+    "</table></body></html>\n")))
